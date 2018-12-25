55caf4712e432e1760173d4928cb02d3e6a9ca8c
Charpell <charpellumeh@gmail.com>
2018-12-25 17:57:12 +0100

    Global Styling and Typography with Styled Components

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,15 +1,25 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => {
   return ( 
-    <div>
-      <Link href="/sell">
-        <a>Sell!</a>
+    <NavStyles>
+      <Link href="/items">
+        <a>Items</a>
       </Link>
       <Link href="/sell">
-        <a>Home!</a>
+        <a>Sell</a>
       </Link>
-    </div>
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+      <Link href="/me">
+        <a>Account</a>
+      </Link>
+    </NavStyles>
    );
 }
  
