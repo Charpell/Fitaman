27ed1f68658a96cb696bd8da9482a2e65d013dd4
Charpell <charpellumeh@gmail.com>
2018-12-27 20:14:46 +0100

    Deleting Items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -24,6 +24,17 @@ const Mutations = {
         id: args.id
       }
     }, info)
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`)
+
+    // 2. Check if they own that item, or have the permissions
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 };
 
