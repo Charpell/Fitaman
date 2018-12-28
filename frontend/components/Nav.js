1a193a32a19942447ac46d6ef4cdb5173ad49dcf
Charpell <charpellumeh@gmail.com>
2018-12-28 15:13:20 +0100

    Sign in Form and Custom Error Handling

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,29 +4,33 @@ import User from './User';
 
 const Nav = () => {
   return ( 
-    <NavStyles>
-      <User>
-        {({ data: { me }}) => {
-          if (me) return <p>{me.name}</p>
-          return null;
-        }}
-      </User>
-      <Link href="/items">
-        <a>Shop</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="/me">
-        <a>Account</a>
-      </Link>
-    </NavStyles>
+    <User>
+      {({ data: { me }}) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+          <Link href="/sell">
+            <a>Sell</a>
+          </Link>
+          <Link href="/orders">
+            <a>Orders</a>
+          </Link>
+          <Link href="/me">
+            <a>Account</a>
+          </Link>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign in</a>
+          </Link>
+        )}
+      </NavStyles>
+      )}
+    </User>
    );
 }
  
--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -5,10 +5,9 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User'
 
-
-const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
-    signup(email: $email, name: $name, password: $password) {
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
       id 
       email 
       name
@@ -16,9 +15,8 @@ const SIGNUP_MUTATION = gql`
   }
 `;
 
-class Signup extends Component {
+class Signin extends Component {
   state = {
-    name: '',
     password: '',
     email: ''
   }
@@ -30,19 +28,19 @@ class Signup extends Component {
   render() { 
     return ( 
       <Mutation 
-        mutation={SIGNUP_MUTATION} 
+        mutation={SIGNIN_MUTATION} 
         variables={this.state}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
         >
-        {(signup, { error, loading }) => (
+        {(signin, { error, loading }) => (
         
         <Form method="post" onSubmit={ async (e) => {
           e.preventDefault();
-          await signup();
+          await signin();
           this.setState({ name: '', email: '', password: '' })
         }}>
           <fieldset disabled={loading} aria-busy={loading}>
-            <h2>Sign Up for An Account</h2>
+            <h2>Sign into your account</h2>
             <Error error={error} />
 
             <label htmlFor="email">
@@ -52,16 +50,6 @@ class Signup extends Component {
                 name="email"
                 placeholder="email"
                 value={this.state.email}
-                onChange={this.saveToState}
-              />
-            </label>
-            <label htmlFor="name">
-              Name
-              <input 
-                type="text"
-                name="name"
-                placeholder="name"
-                value={this.state.name}
                 onChange={this.saveToState}
               />
             </label>
@@ -75,7 +63,7 @@ class Signup extends Component {
                 onChange={this.saveToState}
               />
             </label>
-            <button type="submit">Sign Up!</button>
+            <button type="submit">Sign In!</button>
           </fieldset>
         </Form>  
       )}
@@ -84,4 +72,4 @@ class Signup extends Component {
   }
 }
  
-export default Signup;
+export default Signin;
