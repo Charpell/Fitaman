78b50f807937d654e0bd512007efa088aa23a2e4
Charpell <charpellumeh@gmail.com>
2018-12-29 08:31:31 +0100

    Updating Permissions on the Server

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { hasPermission } = require('../utils');
+
 
 const { transport, makeANiceEmail } = require('../mail')
 
@@ -185,6 +187,35 @@ const Mutations = {
     // return the new user
     return updatedUser;
 
+  },
+
+  async updatePermissions(parent, args, ctx, info) {
+    // check if they are logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+
+    // query the current user
+    const currentUser = await ctx.db.query.user({
+      where: {
+        id: ctx.request.userId
+      }
+    }, info);
+
+    // check if they have permissions to do this
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // update the permissions
+    return ctx.db.mutation.updateUser({
+      data: {
+        permissions: {
+          set: args.permissions
+        }
+      },
+      where: {
+        id: args.userId
+      }
+    }, info)
   }
 };
 
