a33d04a2b45d7d349812932020023bfd60565f15
Charpell <charpellumeh@gmail.com>
2018-12-29 07:30:13 +0100

    Permissions Management

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,16 @@ server.express.use((req, res, next) => {
 })
 
 // TODO Use express middleware to populate current user
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user({ where: { id: req.userId } },
+      '{ id, permissions, email, name }'
+    );
+
+    req.user = user;
+  next()
+})
 
 server.start({
   cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -12,6 +13,19 @@ const Query = {
     return ctx.db.query.user({
       where: { id: ctx.request.userId },
     }, info)
+  },
+
+  async users(parent, args, ctx, info) {
+    // check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+
+    // check if the user has the permission to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+    // if they do, query all the users
+    return ctx.db.query.users({}, info);
   }
 };
 
