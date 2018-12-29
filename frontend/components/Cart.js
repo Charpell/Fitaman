9229564872d417cfe34565de34c15b5a9cc48230
Charpell <charpellumeh@gmail.com>
2018-12-29 08:56:57 +0100

    Creating our cart in React

---
+++ frontend/components/Cart.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+
+
+const Cart = () => {
+  return (
+    <CartStyles open>
+      <header>
+        <CloseButton title="close">&times;</CloseButton>
+        <Supreme>Your Cart</Supreme>
+        <p>You Have __ items in your cart.</p>
+      </header>
+
+      <footer>
+        <p>$10.10</p>
+        <SickButton>Checkout</SickButton>
+      </footer>
+    </CartStyles>
+  )
+}
+
+export default Cart;
